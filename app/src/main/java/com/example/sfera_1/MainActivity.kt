c845d09aa09dbcaa65a378e15c29920ab159e943
t0in4 <t0in4@yahoo.com>
2022-10-28 14:35:02 +0300

    added geoposition button

--- app/src/main/java/com/example/sfera_1/MainActivity.kt
+++ app/src/main/java/com/example/sfera_1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sfera_1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    lateinit var profilename: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewBinding()
+        //val type = Typeface.createFromAsset(assets, "res/font/inter.ttf")
+        //textView.setTypeface(type)
+        //textView.gravity = Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL
     }
+
+    fun viewBinding() {
+        textView = findViewById(R.id.some_id)
+    }
+
 }
