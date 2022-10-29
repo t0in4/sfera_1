3c19bad3566b7c84f23e243544e6f2272fb9746a
t0in4 <t0in4@yahoo.com>
2022-10-29 09:26:05 +0300

    added added second recycler view

---
+++ app/src/main/java/com/example/sfera_1/HorizontalAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.sfera_1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class HorizontalAdapter(val list: ArrayList<Int>) :
+    RecyclerView.Adapter<HorizontalAdapter.MyView>() {
+    class MyView(view: View) : RecyclerView.ViewHolder(view) {
+
+
+        var imageView: ImageView
+        init {
+            imageView = view
+                .findViewById<ImageView>(R.id.image)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
+        val itemView: View = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.recycler_item,
+                parent,
+                false
+            )
+        return MyView(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyView, position: Int) {
+        val listData = list[position]
+        //Glide.with(parent.context).load(list.get(position)).into(holder.imageView)
+
+
+
+        //Loading Image into view
+        //Picasso.get().load(listData).placeholder(R.mipmap.ic_launcher).into(holder.imageView)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+}
--- app/src/main/java/com/example/sfera_1/MainActivity.kt
+++ app/src/main/java/com/example/sfera_1/MainActivity.kt
@@ -1,26 +1,54 @@
 package com.example.sfera_1
 
-import android.graphics.Typeface
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
     lateinit var profilename: TextView
+    lateinit var mRecyclerView: RecyclerView
+    lateinit var mRecyclerView2: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewBinding()
-        //val type = Typeface.createFromAsset(assets, "res/font/inter.ttf")
-        //textView.setTypeface(type)
-        //textView.gravity = Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL
+        // first recycler view - start
+        mRecyclerView?.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL, false)
+        val dataset:ArrayList<Int> = ArrayList()
+        dataset.add(R.drawable.one)
+        dataset.add(R.drawable.two)
+        dataset.add(R.drawable.three)
+        dataset.add(R.drawable.four)
+
+        mRecyclerView.adapter = HorizontalAdapter(dataset)
+        // first recycler view - end
+
+        // second recycler view - start
+        mRecyclerView2?.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL, false)
+        val dataset2:ArrayList<Int> = ArrayList()
+        dataset2.add(R.drawable.one)
+        dataset2.add(R.drawable.two)
+        dataset2.add(R.drawable.three)
+        dataset2.add(R.drawable.four)
+
+        mRecyclerView2.adapter = HorizontalAdapter(dataset2)
+        // second recycler view - end
+
     }
 
     fun viewBinding() {
         textView = findViewById(R.id.some_id)
+        mRecyclerView = findViewById(R.id.horizontalrecyclerview)
+        mRecyclerView2 = findViewById(R.id.horizontalrecyclerview2)
     }
 
 }
